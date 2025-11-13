72e6d1f28dcba493ce1b6868149a6b59e3ab621c
rushuiguan <rushui@outlook.com>
2025-11-13 10:18:20 -0500

    upgrade to dotnet 10

--- alias.ps1
+++ alias.ps1
@@ -1,5 +1,7 @@
 if ($IsMacOS) {
 	set-alias -n admin -v (Join $env:InstallDirectory, "Sample.Admin", "Sample.Admin");
+	set-alias -n test -v (Join $env:InstallDirectory, "Albatross.EFCore.IntegrationTest", "Albatross.EFCore.IntegrationTest");
 } else {
 	set-alias -n admin -v (Join $env:InstallDirectory, "Sample.Admin", "Sample.Admin.exe");
+	set-alias -n test -v (Join $env:InstallDirectory, "Albatross.EFCore.IntegrationTest", "Albatross.EFCore.IntegrationTest.exe");
 }
