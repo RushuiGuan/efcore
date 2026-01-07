a9d6791b71ea02a28acbc6e9f6f2c4300c7f535c
rushuiguan <rushui@gmail.com>
2026-01-07 01:34:49 -0500

    wip

--- Albatross.EFCore.Admin/ExecuteDeploymentScripts.cs
+++ Albatross.EFCore.Admin/ExecuteDeploymentScripts.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System.CommandLine;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,11 +19,9 @@ namespace Albatross.EFCore.Admin {
 		public bool PreMigration { get; set; }
 	}
 
-	public class ExecuteDeploymentScripts<T> : IAsyncCommandHandler where T : IDbSession {
+	public class ExecuteDeploymentScripts<T> : BaseHandler<ExecuteDeploymentScriptsParams> where T : IDbSession {
 		private readonly IExecuteScriptFile executeScriptFile;
 		private readonly T session;
-		private readonly ILogger logger;
-		private readonly ExecuteDeploymentScriptsParams parameters;
 
 		static string FindTargetVersionScriptLocation(string directory) {
 			var version = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
@@ -37,25 +36,25 @@ namespace Albatross.EFCore.Admin {
 			}
 			return Path.Join(directory, version);
 		}
-		
 
-		public ExecuteDeploymentScripts(IExecuteScriptFile executeScriptFile, T session, ILogger logger, ExecuteDeploymentScriptsParams parameters) {
+
+		public ExecuteDeploymentScripts(IExecuteScriptFile executeScriptFile, T session, ILogger<ExecuteDeploymentScripts<T>> logger,
+			ParseResult result, ExecuteDeploymentScriptsParams parameters) : base(result, parameters) {
 			this.executeScriptFile = executeScriptFile;
 			this.session = session;
-			this.logger = logger;
-			this.parameters = parameters;
 		}
 
-		public async Task<int> InvokeAsync(CancellationToken cancellationToken) {
+		public override async Task<int> InvokeAsync(CancellationToken cancellationToken) {
 			if (parameters.PreMigration && !this.session.DbContext.Database.HasPendingModelChanges()) {
-				logger.LogInformation("Skip pre-migration script since there is no pending model changes");
+				await this.Writer.WriteLineAsync("Skip pre-migration script since there is no pending model changes");
 				return 0;
 			} else {
 				var directory = FindTargetVersionScriptLocation(parameters.Directory.FullName);
-				logger.LogInformation("Executing deployment scripts at {location}", directory);
+				await this.Writer.WriteLineAsync($"Found deployment folder: {directory}");
 				var info = new DirectoryInfo(directory);
 				if (info.Exists) {
-					foreach (var file in info.GetFiles("*.sql", SearchOption.TopDirectoryOnly)) {
+					foreach (var file in info.GetFiles("*.sql", SearchOption.TopDirectoryOnly).OrderBy(x => x.Name)) {
+						await this.Writer.WriteLineAsync(file.Name);
 						await this.executeScriptFile.ExecuteAsync(this.session.DbContext, file.FullName, cancellationToken);
 					}
 				}
